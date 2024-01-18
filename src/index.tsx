4da49e7494a7537a11d0ef4404222468ff534748
muxi <yangqinxue333@163.com>
2024-01-18 13:33:18 +0000

    Use hashRouter again

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
@@ -8,19 +8,6 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-// root.render(
-//   <React.StrictMode>
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Homepage />} />
-//         <Route path="/CreateRoomPage" element={<CreateRoomPage />} />
-//         <Route path="/JoinRoomPage" element={<JoinRoomPage />} />
-//         <Route path="/WaitRoomPage" element={<WaitRoomPage />} />
-//         <Route path="/UserProfilePage" element={<UserProfilePage />} />
-//       </Routes>
-//     </Router>
-//   </React.StrictMode>
-// );
 
 root.render(
     <React.StrictMode>
